a20dfb0ff3fcc4de08bdf6a62d635506ff583ccc
ahieu5482 <ahieu5482@gmail.com>
2020-12-21 21:02:40 +0700

    Buoi 6 21/12

---
+++ Code/QuanLiSinhVien/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+namespace QuanLySinhVien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+            ThongTinSinhVien();
+            ThemThongTinSinhVien();
+            Console.WriteLine("--------------------");
+            XoaSinhVien();
+            SuaThongTinSinhVien();
+            XuatDanhSachSinhVien();
+        }
+
+        private static void Menu()
+        {
+            Console.WriteLine("======Phan Mem Quan Ly Sinh Vien======");
+            Console.WriteLine("1. Them Sinh Vien");
+            Console.WriteLine("2. Sua Sinh Vien");
+            Console.WriteLine("3. Xoa Sinh Vien");
+            Console.WriteLine("4. Danh Sach Sinh Vien");
+            Console.WriteLine("exit");
+            Console.WriteLine("Chon:");
+            string Chon = Console.ReadLine();
+            switch (Chon)
+            {
+                case "1":
+                    ThemThongTinSinhVien();
+                    break;
+                case "2":
+                    SuaThongTinSinhVien();
+                    break;
+                case "3":
+                    XoaSinhVien();
+                    break;
+                case "4":
+                    XuatDanhSachSinhVien();
+                    break;
+                case "exit":
+                    return;
+                default:
+                    break;
+            }
+            Menu();
+        }
+
+        private static void ThemThongTinSinhVien()
+        {
+            Console.WriteLine("=====Them Sinh Vien=====");
+            Console.WriteLine("Nhap MaSV:");
+            string maSV = Console.ReadLine();
+            Console.WriteLine("Nhap TenSV:");
+            string tenSV = Console.ReadLine();
+            Console.WriteLine("Nhap DiaChi:");
+            string diaChi = Console.ReadLine();
+            Console.WriteLine("Nhap NgaySinh:");
+            DateTime ngaySinh = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap SDT:");
+            string sDT = Console.ReadLine();
+            SinhVien sv = new SinhVien()
+            {
+                MaSV = maSV,
+                TenSV = tenSV,
+                DiaChi = diaChi,
+                NgaySinh = ngaySinh,
+                SDT = sDT,
+            };
+            sv.ThemDanhSach();
+        }
+
+        private static void SuaThongTinSinhVien()
+        {
+            SinhVien ti = new SinhVien()
+            {
+                MaSV = "002",
+                TenSV = "Tran Thi Ti",
+                DiaChi = "HCM",
+                NgaySinh = new DateTime(2000, 2, 1),
+                SDT = "0123456788"
+            };
+            SinhVien.SuaSinhVien(ti);
+        }
+
+        private static void XuatDanhSachSinhVien()
+        {
+            foreach (SinhVien sv in SinhVien.GetDSSinhVien())
+            {
+                Console.WriteLine(sv.ThongTin());
+            }
+        }
+
+        private static void XoaSinhVien()
+        {
+            string maSV = "001";
+            SinhVien.XoaSinhVien(maSV);
+        }
+
+        private static void ThongTinSinhVien()
+        {
+            // Cach 1
+            SinhVien teo = new SinhVien();
+            teo.MaSV = "001";
+            teo.TenSV = "Teo Nguyen";
+            teo.DiaChi = "HCM";
+            teo.NgaySinh = new DateTime(2000, 1, 1);
+            teo.SDT = "0123456789";
+            //Console.WriteLine(teo.MaSV);
+            //Console.WriteLine(teo.TenSV);
+            //Console.WriteLine(teo.DiaChi);
+            //Console.WriteLine(teo.NgaySinh.ToString());
+            //Console.WriteLine(teo.SDT);
+            Console.WriteLine(teo.ThongTin());
+            //
+            // Cach 2
+            SinhVien ti = new SinhVien()
+            {
+                MaSV = "002",
+                TenSV = "Tran Van Ti",
+                DiaChi = "HCM",
+                NgaySinh = new DateTime(2000, 2, 1),
+                SDT = "0123456788"
+            };
+            Console.WriteLine(ti.ThongTin());
+            SinhVien.GetDSSinhVien();
+            teo.ThemDanhSach();
+            ti.ThemDanhSach();
+            SinhVien tun = new SinhVien()
+            {
+                MaSV = "003",
+                TenSV = "Tran Van Tun",
+                DiaChi = "HCM",
+                NgaySinh = new DateTime(2000, 3, 1),
+                SDT = "0123456777"
+            };
+            SinhVien.ThemDanhSachSV(tun);
+            foreach (SinhVien sv in SinhVien.GetDSSinhVien())
+            {
+                Console.WriteLine(sv.ThongTin());
+            }
+        }   
+    }
+}
---
+++ Code/QuanLiSinhVien/SinhVien.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuanLySinhVien
+{
+    class SinhVien
+    {
+        public string MaSV { get; set; }
+        public string TenSV { get; set; }
+        public string DiaChi { get; set; }
+        public DateTime NgaySinh { get; set; }
+
+        public static void XoaSinhVien(string maSV)
+        {
+            List<SinhVien> danhSachSV = GetDSSinhVien();
+            XoaSinhVienCach1(maSV);
+            XoaSinhVienCach2(maSV);
+        }
+
+        private static void XoaSinhVienCach2(string maSV)
+        {
+            List<SinhVien> dsSV = GetDSSinhVien();
+            dsSV.RemoveAll(sv => sv.MaSV == maSV);
+            DSSinhVien = dsSV;
+        }
+
+        public static void SuaSinhVien(SinhVien svSua)
+        {
+            // Xoa
+            XoaSinhVien(svSua.MaSV);
+            ThemDanhSachSV(svSua);
+        }
+
+        private static void XoaSinhVienCach1(string maSV)
+        {
+            List<SinhVien> dsSV = GetDSSinhVien();
+            int dem = -1;
+            int i = 0;
+            // Tim vi tri sinh vien muon xoa
+            foreach (SinhVien item in dsSV)
+            {
+                if (item.MaSV == maSV)
+                    dem = i;
+                i++;
+            }
+            // Tim duoc thi xoa
+            if (dem > -1)
+                dsSV.RemoveAt(dem);
+            DSSinhVien = dsSV;
+        }
+
+        public string SDT { get; set; }
+        public static List<SinhVien> DSSinhVien;
+        public string ThongTin()
+        {
+            return String.Format("{0},{1},{2},{3},{4}", MaSV, TenSV, DiaChi, NgaySinh, SDT);
+        }
+        /// <summary>
+        /// Lay Danh Sach Sinh Vien
+        /// </summary>
+        /// <returns> </returns>
+        public static List<SinhVien> GetDSSinhVien()
+        {
+            //DSSinhVien = DSSinhVien == null ? new List<SinhVien>() : DSSinhVien;
+            if (DSSinhVien == null)
+                DSSinhVien = new List<SinhVien>();
+            return DSSinhVien;
+        }
+        /// <summary>
+        /// Tu Them Vao Danh Sach
+        /// </summary>
+        public void ThemDanhSach()
+        {
+            SinhVien svThem = this;
+            List<SinhVien> danhSachSV = GetDSSinhVien();
+            danhSachSV.Add(svThem);
+            DSSinhVien = danhSachSV;
+        }
+
+        public void ThemDanhSach(SinhVien svThem)
+        {
+            List<SinhVien> danhSachSV = GetDSSinhVien();
+            danhSachSV.Add(svThem);
+            DSSinhVien = danhSachSV;
+        }
+
+        public static void ThemDanhSachSV(SinhVien svThem)
+        {
+            List<SinhVien> danhSachSV = GetDSSinhVien();
+            danhSachSV.Add(svThem);
+            DSSinhVien = danhSachSV;
+        }
+    }
+}
